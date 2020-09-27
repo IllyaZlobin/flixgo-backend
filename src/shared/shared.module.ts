edd509eb941351630c0f79f35a0bd497fe1b7bd1
illia <illiazlobin@gmail.com>
2020-09-27 12:00:27 +0300

    configured jwt module

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,10 +1,24 @@
 import { Module, HttpModule, Global } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from './services/config.service';
 
+const providers = [ConfigService];
 @Global()
 @Module({
-  providers: [ConfigService],
-  imports: [HttpModule],
-  exports: [ConfigService, HttpModule],
+  providers,
+  imports: [
+    HttpModule,
+    JwtModule.registerAsync({
+      useFactory: (_configService: ConfigService) => ({
+        secretOrPrivateKey: _configService.get('JWT_SECRET_KEY'),
+        signOptions: {
+          expiresIn: _configService.getNumber('JWT_EXPIRATION_TIME'),
+          issuer: 'flixgo',
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  exports: [...providers, HttpModule, JwtModule],
 })
 export class SharedModule {}
