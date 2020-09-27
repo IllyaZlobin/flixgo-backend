a388956bc48a0d0224bee3df8947b812907a610c
illia <illiazlobin@gmail.com>
2020-09-27 03:06:22 +0300

    fixed user seed

--- src/core/typeorm/factories/user.factory.ts
+++ src/core/typeorm/factories/user.factory.ts
@@ -1,15 +1,21 @@
-import { UserRole } from '../../models';
+import { UserRole, UserStatus } from '../../models';
 import { define } from 'typeorm-seeding';
 import { User } from '../entities';
 
 define(User, () => {
   const name = 'illia';
+  const lastName = 'zlobin';
+  const userName = 'illiazlobin';
+  const status = UserStatus.APPROVED;
   const email = 'illiazlobin@gmail.com';
   const password = '123456';
   const role = UserRole.Admin;
 
   const user = new User();
   user.firstName = name;
+  user.lastName = lastName;
+  user.userName = userName;
+  user.status = status;
   user.email = email;
   user.password = password;
   user.role = role;
