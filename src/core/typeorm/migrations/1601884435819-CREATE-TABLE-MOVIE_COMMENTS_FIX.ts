26018bbbe918647f70fd43ff39aa055dfacc338d
illia <illiazlobin@gmail.com>
2020-10-05 10:58:05 +0300

    Create table movie comments migration

---
+++ src/core/typeorm/migrations/1601884435819-CREATE-TABLE-MOVIE_COMMENTS_FIX.ts
@@ -0,0 +1,77 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+import { BasicMigrationProperty } from '../helpers';
+
+const { createdAt, updatedAt } = BasicMigrationProperty;
+
+export class CREATETABLEMOVIECOMMENTSFIX1601884435819
+  implements MigrationInterface {
+  tableName = 'movie_comments';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: this.tableName,
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isNullable: false,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'text',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'userId',
+            type: 'int',
+            isNullable: false,
+          },
+          {
+            name: 'movieId',
+            type: 'int',
+            isNullable: false,
+          },
+          createdAt,
+          updatedAt,
+        ],
+        foreignKeys: [
+          {
+            columnNames: ['userId'],
+            name: 'FX_MOVIE_COMMENTS_USERID',
+            referencedTableName: 'user',
+            referencedColumnNames: ['id'],
+            onDelete: 'cascade',
+            onUpdate: 'cascade',
+          },
+          {
+            columnNames: ['movieId'],
+            name: 'FK_MOVIE_COMMENTS_MOVIEID',
+            referencedTableName: 'movie',
+            referencedColumnNames: ['id'],
+            onDelete: 'cascade',
+            onUpdate: 'cascade',
+          },
+        ],
+        indices: [
+          {
+            columnNames: ['text'],
+            name: 'IDX_MOVIE_COMMENTS_TEXT',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropIndex(this.tableName, 'IDX_MOVIE_COMMENTS_TEXT');
+    await queryRunner.dropForeignKey(
+      this.tableName,
+      'FK_MOVIE_COMMENTS_MOVIEID',
+    );
+    await queryRunner.dropTable(this.tableName);
+  }
+}
